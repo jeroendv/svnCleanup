6ce9355e10705bd1f67b346963cb221b57fa3895
jeroendv <fly.jdv@gmail.com>
2018-07-31 23:41:54 +0200

    add some more tesy

--- test_svncleanup.py
+++ test_svncleanup.py
@@ -2,6 +2,7 @@ import os
 
 import svncleanup
 import sys
+import subprocess
 
 def test(tmpdir):
     tmpdir = str(tmpdir)
@@ -10,9 +11,25 @@ def test(tmpdir):
     print("pwd: " + os.getcwd())
 
 
-def test_svnclenaupModuleInvocation(tmpdir):
+def test_svncleanupModuleInvocation(tmpdir):
     tmpdir = str(tmpdir)
     os.chdir(tmpdir)
+
+    # set cli arg and invode script main
+    # which should not raise
+    sys.argv = [svncleanup.__name__]
+    svncleanup.main()
+
+    # verify that log file is created
+    assert os.path.isfile('svnCleanup.log')
+
+def test_svncleanupModuleInvocation2(tmpdir):
+    tmpdir = str(tmpdir)
+    os.chdir(tmpdir)
+
+    # create an svn repo and check it out
+    subprocess.check_call('svnadmin create svnRepo', shell=True)
+    subprocess.check_call('svn checkout file://{}/svnRepo svnWc'.format(tmpdir), shell=True)
 
     # set cli arg and invode script main
     # which should not raise
