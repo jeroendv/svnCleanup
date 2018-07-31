b68a02f29d847e139a074f248da974dea6100fa2
jeroendv <fly.jdv@gmail.com>
2018-07-31 22:54:21 +0200

    add first test

--- test_svncleanup.py
+++ test_svncleanup.py
@@ -1,6 +1,22 @@
 import os
 
+import svncleanup
+import sys
+
 def test(tmpdir):
-    print(tmpdir)
-    print(os.path.isdir(tmpdir))
+    print("tmpdir: " + str(tmpdir))
+    print("tmpdir is dir :" + str(os.path.isdir(tmpdir)))
+    print("pwd: " + os.getcwd())
+
+
+def test_svnclenaupModuleInvocation(tmpdir):
+    os.chdir(tmpdir)
+
+    # set cli arg and invode script main
+    # which should not raise
+    sys.argv = [svncleanup.__name__]
+    svncleanup.main()
+
+    # verify that log file is created
+    assert os.path.isfile('svnCleanup.log')
 
